c6f350a67e4e2059071cecc2c6e1bbd1790ccedb
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-10-28 16:25:53 -0500

    act

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+// config/db.js
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Conectado a MongoDB Atlas'))
+    .catch((error) => console.error('Error conectando a MongoDB:', error));
+
+module.exports = mongoose;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Importar la rutas
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`);})
+
