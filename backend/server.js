1f2d8f713faafbf4217818fb4158812ad6ca9903
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-10-28 16:50:49 -0500

    act

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,6 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
-    console.log(`Server running on port ${PORT}`);})
+    console.log(`Server running on port ${PORT}`);
+})
 
