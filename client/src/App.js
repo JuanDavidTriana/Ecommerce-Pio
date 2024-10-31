e505c3fd8c2e81f51dece0cc4ecefe0a3fac68ec
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-10-31 15:52:05 -0500

    act

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //Pages
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 //Components
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </Router>
   );
