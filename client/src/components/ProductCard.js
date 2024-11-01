16f917f5dc32d3a3f24ecac555dcc564e2967f22
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-11-01 17:15:11 -0500

    act

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -7,6 +7,7 @@ const ProductCard = ({ product }) => {
             <h3>{product.name}</h3>
             <p>{product.description}</p>
             <p>{product.price}</p>
+            <p>{product.category}</p>
             <img src={product.imageUrl} alt={product.name} />
             <Link to={`/products/${product._id}`}>View</Link>
         </div>
